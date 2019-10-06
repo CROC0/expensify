ccb31ee8f7b4812239d95d13771ea19c1a65c58d
Matt Taylor <matttaylor89@live.com.au>
2019-10-06 20:16:51 +0800

    setup crud functions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid'
+
 import db from '../firebase/firebase'
 
 // ACTIONS
@@ -38,3 +38,27 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const setExpenses = (expenses) => ({
+  type: "SET_EXPENSES",
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return db.ref('expenses')
+      .once('value')
+      .then((snapshot) => {
+        const expenses = [];
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
+        });
+        dispatch(setExpenses(expenses))
+      }
+      )
+  };
+};
+
