f99d8812edc2603614bb11640ffa2a07665590b0
Matt Taylor <matttaylor89@live.com.au>
2019-10-13 16:48:29 +0800

    Add Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses';
 import getExpenseTotal from '../selectors/expenses-total'
 
-export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
-    const amount = numeral(expensesTotal / 100).format('$0,0.00')
-    return (
-    <div>
-    <h1>
-    Viewing {expenseCount} {expenseWord} totalling {amount}
-    </h1>
-    
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+  const amount = numeral(expensesTotal / 100).format('$0,0.00')
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{amount}</span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
-)};
+  )
+};
 
 const mapStateToProps = (state) => {
-    const expenses = selectExpenses(state.expenses, state.filters)
-    return {
-      expenseCount: expenses.length,
-      expensesTotal: getExpenseTotal(expenses)
-    };
+  const expenses = selectExpenses(state.expenses, state.filters)
+  return {
+    expenseCount: expenses.length,
+    expensesTotal: getExpenseTotal(expenses)
   };
-  
-  export default connect(mapStateToProps)(ExpensesSummary);
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,17 +3,17 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-            <button onClick={startLogin}>
-                Login
-            </button>
-    
-
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify</h1>
+      <p>Login to enter your expenses</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
+  </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+  startLogin: () => dispatch(startLogin())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage)
